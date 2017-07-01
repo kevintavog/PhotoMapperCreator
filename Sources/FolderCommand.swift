f6503fed1ba0ae36cd8f86354fc51ff79bdbf8d1
kevintavog <kevin.tavog@zoho.com>
2017-06-30 17:23:01 -0700

    Fix an issue causing folders to be processed more than once

--- Sources/FolderCommand.swift
+++ Sources/FolderCommand.swift
@@ -18,12 +18,12 @@ print("Make sure exiftool, vipsthumbnail & ffmpeg are available")
             } catch {
                 throw CLIError.error("Bad input folder: \(error)")
             }
+        }
 
-            do {
-                try Processor.instance.execute(outputFolder: outputFolder)
-            } catch {
-                throw CLIError.error("Failed processing: \(error)")
-            }
+        do {
+            try Processor.instance.execute(outputFolder: outputFolder)
+        } catch {
+            throw CLIError.error("Failed processing: \(error)")
         }
     }
 }
